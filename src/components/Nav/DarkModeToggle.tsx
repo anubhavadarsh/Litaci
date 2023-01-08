5c4e032f1d56759ea1b58df44ff3a34d960915dc
Anubhav <anubhav.adarsh9@gmail.com>
2023-01-08 11:42:18 +0530

    refactor: animate dark mode toggle

--- src/components/Nav/DarkModeToggle.tsx
+++ src/components/Nav/DarkModeToggle.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext } from 'react';
 import clsx from 'clsx';
+import { motion, useTime, useTransform } from 'framer-motion';
 
 import { themeCtx } from 'context/theme-context';
 import styles from './DarkModeToggle.module.scss';
@@ -11,6 +12,14 @@ interface props {
 const DarkModeToggle: FC<props> = ({ className: classProps }) => {
   const tCtx = useContext(themeCtx);
 
+  const time = useTime();
+  const rotate = useTransform(
+    time,
+    [0, 4000], // For every 4 seconds...
+    [0, 360], // ...rotate 360deg
+    { clamp: false }
+  );
+
   return (
     <label className={clsx(styles.cont, classProps)}>
       <input
@@ -20,15 +29,27 @@ const DarkModeToggle: FC<props> = ({ className: classProps }) => {
         onChange={tCtx.toggleDark}
         checked={tCtx.dark}
       />
-      <span
-        className={clsx(
-          'material-symbols-outlined',
-          tCtx.dark && styles.__active
-        )}>
+      <motion.span
+        variants={variants}
+        animate={tCtx.dark ? 'dark' : 'light'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{ rotate }}
+        className={clsx('material-symbols-outlined')}>
         light_mode
-      </span>
+      </motion.span>
     </label>
   );
+};
+
+const variants = {
+  light: {
+    y: '10px',
+  },
+  dark: {
+    y: 0,
+  },
 };
 
 export default DarkModeToggle;
