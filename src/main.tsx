c073f555a766f8852f080536bfccaaec24bc938d
Anubhav <anubhav.adarsh9@gmail.com>
2023-02-04 18:13:07 +0530

    feat: creat about page

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from 'router';
-import App from './App';
 import 'normalize.css';
 import 'styles/main.scss';
 
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,30 @@
-import Page from 'containers/Page';
 import { FC } from 'react';
+import clsx from 'clsx';
+
+import Page from 'containers/Page';
+import Banner from 'containers/Banner';
+import styles from './About.module.scss';
 
 const AboutPage: FC = () => {
   return (
-    <Page>
-      <h1>About me</h1>
+    <Page className={styles.about}>
+      <Banner
+        main='About'
+        className={clsx(styles.banner, styles.bannerBottom)}
+      />
+      <div className={styles.content}>
+        <div className={clsx(styles.card, styles.left)}></div>
+        <div
+          className={styles.card}
+          style={{ left: '60%', height: '40%', top: '50%' }}></div>
+        <div
+          className={styles.card}
+          style={{ left: '60%', height: '40%', top: '150%' }}></div>
+      </div>
+      <Banner
+        main='About'
+        className={clsx(styles.banner, styles.bannerTop)}
+      />
     </Page>
   );
 };
