cbe91229824bd5d425722f1a7f19cb030004a2bd
Anubhav <anubhav.adarsh9@gmail.com>
2023-02-14 19:01:04 +0530

    feat: add mouse and touch event support to page container

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -4,16 +4,36 @@ import clsx from 'clsx';
 
 import styles from './Page.module.scss';
 
-interface Props {
+interface Props extends HTMLMotionProps<'div'> {
   motionProps?: HTMLMotionProps<'div'>;
   children: ReactNode;
   className?: string;
 }
 
+//TODO: Fix the remove forward ref with motion() refer: https://www.framer.com/motion/component/
 const Page = forwardRef<HTMLDivElement, Props>(
-  ({ motionProps, className, children }, ref) => {
+  (
+    {
+      motionProps,
+      className,
+      children,
+      onMouseDown,
+      onMouseMove,
+      onMouseUp,
+      onTouchMove,
+      onTouchStart,
+      onTouchEnd,
+    },
+    ref
+  ) => {
     return (
       <motion.div
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onTouchMove={onTouchMove}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
         {...motionProps}
         className={clsx(styles.Page, className)}
         ref={ref}>
