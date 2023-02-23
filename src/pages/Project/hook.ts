6491621bfa0d72fb224ee38820c92cca506746b1
Anubhav <anubhav.adarsh9@gmail.com>
2023-02-23 16:18:26 +0530

    feat: create hooks for api calls for languages and readme content

--- src/pages/Project/hook.ts
+++ src/pages/Project/hook.ts
@@ -1,4 +1,6 @@
 import { useState, useRef, RefObject } from 'react';
+import { Repos } from 'services/api/Repos';
+import { extractContentType } from './util';
 
 const usePointerMovement = (ref: RefObject<HTMLElement>) => {
   const [mouseMovement, setMouseMovement] = useState(0);
@@ -38,4 +40,84 @@ const usePointerMovement = (ref: RefObject<HTMLElement>) => {
   };
 };
 
-export { usePointerMovement };
+class RepoUnknownError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'RepoUnkownError';
+    this.message = message;
+  }
+}
+
+const useGetReadme = () => {
+  const [readMe, setReadMe] = useState<string>('');
+
+  const makeRequest = async (
+    config: { repo: string; ac: AbortController },
+    request: typeof Repos.singleContentReadme
+  ) => {
+    try {
+      const response = await request(config.repo, config.ac.signal);
+
+      if (response.status === 404) {
+        throw new RepoUnknownError(
+          "this repository doesn't have a README.md file."
+        );
+      }
+
+      const ct = response.headers.get('content-type');
+      if (ct) {
+        const contentType = extractContentType(ct);
+        if (contentType === 'json') {
+          const jsonFormat = await response.json();
+          setReadMe(atob(jsonFormat.content));
+        } else if (contentType === 'vnd.github.raw') {
+          const textFormat = await response.text();
+          setReadMe(textFormat);
+        }
+      }
+    } catch (err) {
+      if (err instanceof RepoUnknownError) {
+        setReadMe(err.message);
+      }
+    }
+  };
+
+  return { makeRequest, readMe };
+};
+
+type Lang = {
+  name: string;
+  code: number;
+};
+
+const useGetLang = () => {
+  const [langs, setLangs] = useState<Lang[]>([]);
+
+  const makeRequest = async (
+    config: { repo: string; ac: AbortController },
+    request: typeof Repos.singleContentReadme
+  ) => {
+    try {
+      const response = await request(config.repo, config.ac.signal);
+      const jsonFormat = (await response.json()) as { [key: string]: number };
+
+      let l: Lang[] = [];
+      for (const a in jsonFormat) {
+        l.push({
+          name: a,
+          code: jsonFormat[a],
+        });
+      }
+      setLangs(l);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return {
+    makeRequest,
+    langs,
+  };
+};
+
+export { usePointerMovement, useGetReadme, useGetLang };
--- src/pages/Project/util.ts
+++ src/pages/Project/util.ts
@@ -19,3 +19,32 @@ export const loader = async ({
     return json(data);
   }
 };
+
+export const getColor = (seed: string) => {
+  const m = new Map([
+    ['javascript', '#f1e05a'],
+    ['html', '#e34c26'],
+    ['scss', '#c6538c'],
+    ['css', '#563d7c'],
+    ['typescript', '#3178c6'],
+    ['c', '#555555'],
+    ['c++', '#f34b7d'],
+    ['solidity', '#AA6746'],
+    ['purebasic', '#5a6986'],
+  ]);
+
+  const color = m.get(seed.toLowerCase());
+
+  return color === undefined ? '#c6538c' : color;
+};
+
+export const extractContentType = (ct: string) => {
+  const pattern = /(?<=application\/)([\w.-]+)(?=;)/g;
+  const match = pattern.exec(ct);
+
+  if (match) {
+    return match[1];
+  }
+
+  return null;
+};
